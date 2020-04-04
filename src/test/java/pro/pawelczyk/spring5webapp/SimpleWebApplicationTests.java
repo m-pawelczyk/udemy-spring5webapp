29fa637ae8e5a94fee9970b94fc1f862d5e6d244
m-pawelczyk <m-pawelczyk@users.noreply.github.com>
2020-04-04 13:33:12 +0200

    Demo project created

---
+++ src/test/java/pro/pawelczyk/spring5webapp/SimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
